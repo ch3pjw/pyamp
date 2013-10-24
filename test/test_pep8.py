adb44361d3802fca3643d2c2053b0fbc4317046c
Paul Weaver <paul@ruthorn.co.uk>
2013-10-24 19:33:43 +0100

    Added pep8_config file to tune pep8 testing

--- test/test_pep8.py
+++ test/test_pep8.py
@@ -7,7 +7,10 @@ class TestPep8Conformance(TestCase):
     def test_pep8_conformance(self):
         cur_dir_path = os.path.dirname(__file__)
         root_path = os.path.join(cur_dir_path, os.pardir)
-        pep8_style = pep8.StyleGuide(paths=[root_path])
+        config_path = os.path.join(cur_dir_path, 'pep8_config')
+        pep8_style = pep8.StyleGuide(
+            paths=[root_path],
+            config_file=config_path)
         result = pep8_style.check_files()
         self.assertEqual(
             result.total_errors, 0,
