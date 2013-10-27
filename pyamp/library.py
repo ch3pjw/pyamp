879c3400f3820dee03dd9b32c620ba1d13508412
Paul Weaver <paul@ruthorn.co.uk>
2013-10-27 17:05:49 +0000

    Made paymp only create database tables if necessary...
    and not drop existing ones. However, WARNING!, we don't do anything smart yet
    to update the database only when required, we just keep adding tracks to it
    every run, so don't use this commit in production!

--- pyamp/library.py
+++ pyamp/library.py
@@ -54,24 +54,15 @@ class Library(PyampBase):
         float: 'SINGLE'}
     Metadata = namedtuple('Metadata', _metadata_format.keys())
     _metadata_placeholder = ', '.join('?' * len(_metadata_format))
+    _metadata_sql_spec = [
+        (name, _format_to_sql_spec[format_]) for name, format_ in
+        _metadata_format.iteritems()]
 
     def __init__(self, database_file):
         super(Library, self).__init__()
         self.database_file = os.path.expanduser(database_file)
         from gst import pbutils
         self.discoverer = pbutils.Discoverer(gst.SECOND)
-
-    @property
-    def _metadata_sql_spec(self):
-        '''We want this to be a class variable, but we can't easily calculate
-        it in the class definition because of name scoping. So, lets make a
-        property that replaces itself on first call.
-        '''
-        metadata_sql_spec = ', '.join('{name} {type_}'.format(
-            name=name, type_=self._format_to_sql_spec[format_]) for
-            name, format_ in self._metadata_format.iteritems())
-        self.__class__._metadata_sql_spec = metadata_sql_spec
-        return metadata_sql_spec
 
     def _make_metadata(self, file_path, gst_tags):
         tag_dict = {}
@@ -93,18 +84,39 @@ class Library(PyampBase):
         self.log.debug('Found file {}'.format(metadata))
         return metadata
 
+    def _create_table_if_required(self, cursor, table_name, spec):
+        cursor.execute('PRAGMA table_info({})'.format(table_name))
+        table_schema = cursor.fetchall()
+        if table_schema:
+            self.log.debug('Found existing {} table...'.format(table_name))
+            existing_spec = []
+            for column in table_schema:
+                id_, name, type_, _, _, _ = column
+                existing_spec.append((name, type_))
+            if existing_spec == spec:
+                self.log.debug(
+                    '{} table conforms to schema'.format(table_name))
+                return
+            else:
+                self.log.warning(
+                    '{} table does not conform to schema, dropping'.format(
+                        table_name))
+                cursor.execute('DROP TABLE {}'.format(table_name))
+        self.log.debug('Creating new {} table'.format(table_name))
+        spec_string = ', '.join(
+            '{} {}'.format(name, type_) for name, type_ in spec)
+        cursor.execute('CREATE TABLE {}({})'.format(table_name, spec_string))
+
     @blocking
     @with_database_cursor
     def discover_on_path(self, cursor, dir_path):
         dir_path = os.path.expanduser(dir_path)
         self.log.info('Discovering tracks on {}'.format(dir_path))
-        # FIXME: eventually, of course, we'll want data to persist
-        cursor.execute('DROP TABLE IF EXISTS Dirs')
-        cursor.execute(
-            'CREATE TABLE Dirs(dir_path TEXT, modified_time SINGLE)')
-        cursor.execute('DROP TABLE IF EXISTS Tracks')
-        cursor.execute('CREATE TABLE Tracks({})'.format(
-            self._metadata_sql_spec))
+        self._create_table_if_required(
+            cursor, 'Dirs',
+            [('dir_path', 'TEXT'), ('modified_time', 'SINGLE')])
+        self._create_table_if_required(
+            cursor, 'Tracks', self._metadata_sql_spec)
         for cur_dir_path, sub_dir_names, file_names in os.walk(dir_path):
             dir_stats = os.stat(cur_dir_path)
             cursor.execute(
@@ -124,7 +136,7 @@ class Library(PyampBase):
 
     def _get_search_query(self, search_string):
         searchable_fields = 'artist', 'album', 'title'
-        query = 'SELECT DISTINCT file_path FROM Tracks WHERE '
+        query = 'SELECT file_path FROM Tracks WHERE '
         search_portion = "{field} LIKE '%{search_string}%'"
         search_portions = ' OR '.join(search_portion.format(
             field=field, search_string=search_string) for field in
