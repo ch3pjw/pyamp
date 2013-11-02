c40ae186dd10584a6db406c1a87d0fdfb4735ba9
Paul Weaver <paul@ruthorn.co.uk>
2013-11-02 00:27:32 +0000

    Updated setup.py for Python3...
    most notably, I realised that sqlite3 bundled, so we don't need to require
    pysqlite. I also replaced Twisted with asyncio (formerly tulip), which will
    render the app totally dead, but will hopefully be shiny fun to use :-)

--- setup.py
+++ setup.py
@@ -3,27 +3,29 @@ import sys
 from setuptools import setup
 
 try:
-    print 'Testing gstreamer python support...'
-    import pygst
-    pygst.require('0.10')
-    import gst
+    print('Testing gstreamer python support...')
+    import gi
+    gi.require_version('Gst', '1.0')
+    from gi.repository import Gst
 except ImportError:
     sys.exit("Cannot install pyamp, no gstreamer python bindings present")
+
+print('Found gstreamer python bindings, gstreamer version is {}'.format(
+    '.'.join(str(n) for n in Gst.version())))
 
 __version__ = None
 # Populate __version__ using _pyamp._version module, without importing
 this_dir_path = os.path.dirname(__file__)
 version_module_path = os.path.join(this_dir_path, 'pyamp', '_version.py')
-exec(file(version_module_path).read())
+exec(open(version_module_path).read())
 
 setup(
     name='pyamp',
     version=__version__,
     install_requires=[
-        'Twisted',
+        'asyncio',
         'blessings',
-        'pyyaml',
-        'pysqlite'],
+        'pyyaml'],
     packages=[
         'pyamp'],
     package_data={
